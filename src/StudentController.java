b9704f949a35eb3b23670b05d4b7b03d3990636f
Alex Aimar Diaz De La Cruz <alex.diaz.d@tecsup.edu.pe>
2026-03-24 23:38:19 -0500

    cambios

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,5 +1,11 @@
 public class StudentController {
     public static void main(String[] args) {
         System.out.println("Controlador de estudiantes");
+        greetStudent("Alex");
+    }
+
+    // Método simple para saludar a un estudiante
+    public static void greetStudent(String name) {
+        System.out.println("¡Hola, " + name + "! Bienvenido al sistema de estudiantes.");
     }
 }
